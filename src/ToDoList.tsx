0914a5f55ffdc6e5bebecb18980061d23f116c70
yjkim0109 <yjkkim0109@gmail.com>
2022-10-03 17:23:34 +0900

    Feat: Reset 및 recoil 적용

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,137 +1,63 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
+import {
+	atom,
+	useRecoilState,
+	useRecoilValue,
+	useSetRecoilState,
+} from 'recoil';
 
-interface IForm {
-	email: string;
-	firstName: string;
-	lastName: string;
-	userName: string;
-	password: string;
-	password1: string;
-	extraError?: string;
+const toDoState = atom<IToDo[]>({
+	key: 'toDo',
+	default: [],
+});
+
+interface Iform {
+	toDo: string;
+}
+
+interface IToDo {
+	text: string;
+	id: number;
+	category: 'TO_DO' | 'DOING' | 'DONE';
 }
 
 function ToDoList() {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		setError,
-	} = useForm<IForm>({
-		defaultValues: {
-			email: '@naver.com',
-		},
-	});
+	const [toDos, setToDos] = useRecoilState(toDoState);
+	// value만 받고 싶을때 const value = useRecoilValue(toDoState);
+	// value를 수정하고 싶을때 const modFn = useSetRecoilState(toDoState);
 
-	const onValid = (data: IForm) => {
-		if (data.password !== data.password1) {
-			setError(
-				'password1',
-				{ message: 'Password are not the same' },
-				{ shouldFocus: true }
-			);
-		}
-		// setError('extraError', { message: 'server offline' });
-	};
-	// console.log(formState?.errors);
-
-	return (
-		<div>
-			<form
-				style={{ display: 'flex', flexDirection: 'column' }}
-				onSubmit={handleSubmit(onValid)}>
-				<input
-					{...register('email', {
-						required: 'Email is required',
-						pattern: {
-							value: /^[A-Za-z0-9._%+-]+@naver\.com$/,
-							message: 'Only naver.com emails allowed.',
-						},
-					})}
-					placeholder='Email'
-				/>
-				<span>{errors?.email?.message}</span>
-
-				<input
-					{...register('firstName', {
-						required: 'write here',
-						validate: {
-							noNico: (value) =>
-								value.includes('nico') ? 'no nicos allowed' : true,
-							noNick: (value) =>
-								value.includes('nick') ? 'no nicks allowed' : true,
-						},
-					})}
-					placeholder='First Name'
-				/>
-				<span>{errors?.firstName?.message}</span>
-
-				<input
-					{...register('lastName', { required: 'write here' })}
-					placeholder='Last Name'
-				/>
-				<span>{errors?.lastName?.message}</span>
-
-				<input
-					{...register('userName', { required: 'write here', minLength: 5 })}
-					placeholder='User Name'
-				/>
-				<span>{errors?.userName?.message}</span>
-
-				<input
-					{...register('password', { required: 'write here', minLength: 5 })}
-					placeholder='Password'
-				/>
-				<span>{errors?.password?.message}</span>
-
-				<input
-					{...register('password1', {
-						required: 'Password is required',
-						minLength: {
-							value: 5,
-							message: 'Your password is too short',
-						},
-					})}
-					placeholder='Password1'
-				/>
-				<span>{errors?.password1?.message}</span>
-				<button>Add</button>
-				<span>{errors?.extraError?.message}</span>
-			</form>
-		</div>
-	);
-}
-
-/*
-function ToDoList() {
-	const [Todo, setTodo] = useState('');
-	const [TodoError, setTodoError] = useState("");
-	const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-		const {
-			currentTarget: { value },
-		} = event;
-		setTodoError("");
-		setTodo(value);
-	};
-	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-	if(Todo.length < 10) {
-	return setTodoError("To do should be longer.");
-	}
-	console.log("submit");
+	const { register, handleSubmit, setValue } = useForm<Iform>();
+	const handleValid = ({ toDo }: Iform) => {
+		setToDos((oldToDos) => [
+			{ text: toDo, id: Date.now(), category: 'TO_DO' },
+			...oldToDos,
+		]);
+		setValue('toDo', '');
 	};
 
 	return (
 		<div>
-			<form onSubmit={onSubmit}>
-				<input onChange={onChange} value={Todo} placeholder='Write a to do' />
+			<h1>To Dos</h1>
+			<hr />
+			<form onSubmit={handleSubmit(handleValid)}>
+				<input
+					{...register('toDo', {
+						required: 'Please write a to do.',
+					})}
+					placeholder='Write a to do'
+				/>
 				<button>Add</button>
-				{TodoError !== "" ? TodoError : null}
 			</form>
+
+			<ul>
+				{toDos.map((toDo) => (
+					<li key={toDo.id}>{toDo.text}</li>
+				))}
+			</ul>
 		</div>
 	);
 }
-*/
 
 export default ToDoList;
